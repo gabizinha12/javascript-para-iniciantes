9b97c0cab05b6f1f882d0b340c0c4a7f22d1fecf
gabizinha12 <gabrielle.plima1965@gmail.com>
2020-07-23 18:31:18 -0300

    arquivos da aula 1

---
+++ variaveis/script.js
@@ -0,0 +1,32 @@
+var nome = 'Gabi'
+var idade = 19
+console.log(nome, idade)
+
+var preco = 25
+var totalComprado = 5
+var totalPreco = totalComprado * preco
+console.log(totalPreco)
+
+var sobrenome = 'Lima', 
+cidade = 'Paraná'
+console.log(sobrenome, cidade)
+
+var semDefinir;
+console.log(semDefinir)
+
+// os nomes podem iniciar com $, _ ou letras
+// podem conter numeros mas nao iniciar com eles
+// case sensitive
+// nao usar palavras reservadas
+// camelCase
+
+// Inválido
+// var §nome;
+// var function;
+// var 1possuiFaculdade;
+
+// Válido
+var $selecionar;
+var _nome;
+var possuiFaculdadeNoExterior;
+
